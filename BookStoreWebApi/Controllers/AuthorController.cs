01caf43ecd1a17145667b448e1f2f67229883f7b
Rizwan Khan <riz_wkhan@hotmail.ca>
2020-05-29 01:51:18 -0400

    Add project files.

---
+++ BookStoreWebApi/Controllers/AuthorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStoreWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Author> Get()
+        {
+            using (var context = new BookStoresDBContext())
+            {
+                // get all author
+                //return context.Author.ToList();
+
+                // get author by id
+                return context.Author.Where(au => au.AuthorId == 1).ToList();
+            }
+
+        }
+    }
+}
---
+++ BookStoreWebApi/Models/Job.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookStoreWebApi.Models
+{
+    public partial class Job
+    {
+        public Job()
+        {
+            User = new HashSet<User>();
+        }
+
+        public short JobId { get; set; }
+        public string JobDesc { get; set; }
+
+        public virtual ICollection<User> User { get; set; }
+    }
+}
